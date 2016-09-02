968f339b0950d6df1246e0645acc8c2266f50483
sankit acharya <sankit.acharya@gmail.com>
2016-09-03 02:20:02 +0530

    MeidaWiki API added

--- Ajax_project/minicourse-ajax-project/js/script.js
+++ Ajax_project/minicourse-ajax-project/js/script.js
@@ -38,6 +38,26 @@ function loadData() {
     }).error(function(e) {
       $nytHeaderElem.text('New York Times Article Could Not be Loaded');
     });
+
+    var wikiRequestTimeout = setTimeout(function() {
+      $wikiElem.text('failed to get wikipedia resources');
+    },8000);
+
+    var wikiurl = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+ cityStr +"&format=json&callback=wikiCallback";
+    $.ajax({
+      url :wikiurl,
+      dataType:"jsonp",
+      // jsonp :"callback",
+      success: function(response) {
+      var articleList = response[1];
+      for(var i =0 ; i<articleList.length ; i++){
+        var articleStr = articleList[i];
+        var url = "http://en.wikipedia.org/wiki/" + articleStr;
+        $wikiElem.append('<li><a href="'+url+'">'+articleStr+'</a></li>');
+      };
+      clearTimeout(wikiRequestTimeout);
+    }
+  });
     return false;
 };
 
