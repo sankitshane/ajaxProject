322742b59953448fd1882b90fec2941e584c1daa
sankit acharya <sankit.acharya@gmail.com>
2016-09-03 01:29:58 +0530

    NY times API Added

--- Ajax_project/minicourse-ajax-project/js/script.js
+++ Ajax_project/minicourse-ajax-project/js/script.js
@@ -12,9 +12,32 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr +','+ cityStr;
+    $('.greeting').text('So, you wanna live at'+ address + '?');
+    $body.append('<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location= '+address+'">');
     // YOUR CODE GOES HERE!
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'q' : cityStr,
+      'api-key': "1fabcf73aa33430db64ff29eca489e43"
+    });
+    $.getJSON(url,function(data) {
+      $nytHeaderElem.text('New YorkTimes Article About' + cityStr);
 
+      articles = data.response.docs;
+      for(var i = 0;i < articles.length ;i++) {
+        var article = articles[i];
+        $nytElem.append('<li class="article">'+
+                        '<a href="' + article.web_url+'">'+article.headline.main+'</a>'
+                        +'<p>'+article.snippet+'</p>'+
+                          '</li>');
+      };
+
+    }).error(function(e) {
+      $nytHeaderElem.text('New York Times Article Could Not be Loaded');
+    });
     return false;
 };
 
